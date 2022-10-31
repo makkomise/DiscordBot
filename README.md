4810921dd2acf3b2ecada994e32a916d0166c792
Mikko Turunen <57153670+makkomise@users.noreply.github.com>
2022-10-31 20:11:00 +0200

    Initial D
    lesssgoooo
    
    Includes:
    - Basic functionality
    - Adding words to list
    - Deletes prohibited words

---
+++ README.md
@@ -0,0 +1,4 @@
+# DiscordBot
+Just a simple bot for Discord
+
+Making this as a sideproject for school / later going to implement in real use
---
+++ main.py
@@ -0,0 +1,62 @@
+import discord
+import os
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot = commands.Bot(command_prefix='!', help_command=None)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')  
+
+
+#tuhmien sanojen filtteri
+
+with open ('blacklist.txt', 'r') as file:  #jos joku kirjoittaa sanan mikä filtterissä, botti reagoi
+    blacklist = file.read().split("\n")
+
+@bot.event 
+async def on_message(message):
+
+    if message.author == bot.user:
+        return
+
+    for word in blacklist:
+        if word in message.content.lower():
+            await message.delete()  #poistaa tuhman viestin
+            await message.channel.send(f' {message.author.mention} your message was removed due to channel rules' )    #lähettää tämmöttisen viestin jos sana listalla
+      
+    await bot.process_commands(message)
+
+
+
+@bot.command(name='help')   #botin helppikomento, miten lisätä sanoja kirosanafiltteriin
+async def help(ctx):   
+    await ctx.send('`!add xxxxxx to add words to blacklist`')
+
+
+#BOTIN KOMENNOT 
+
+@bot.command(name='carl')   #hassunhauska testikomento, pyydettiin laittamaan
+async def carl(message):   
+    await message.send('CAAAAAAAAAARRRLLLLLLLLLLLLLL')
+
+@bot.command(name='add')    #sanojen lisääminen kirosanafiltteriin
+async def add(message, arg):
+    if message.message.author.guild_permissions.administrator:
+        word = arg
+        file = open('blacklist.txt', 'a+')
+        file.write("\n" + word)
+        file.close()
+        await message.send('Word '+ word +' has been added to blacklist.')
+
+    else:
+        await message.send('You shall not pass')
+
+
+    
+
+bot.run(TOKEN)
